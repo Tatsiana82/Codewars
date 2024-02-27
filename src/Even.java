1df8ad91be6ac96c5f58660a0ffc99b3937aa513
Tatsiana82 <70721957+Tatsiana82@users.noreply.github.com>
2024-02-27 21:20:16 +0300

    Add

---
+++ src/Even.java
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+public class Even {
+    public static void main(String[] args) {
+        System.out.println(Kata.evenOrOdd(5));
+    }
+
+    class Kata {
+        public static String evenOrOdd(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+
+            }
+            return "Odd";
+        }
+    }
+}
+
+
+
+
