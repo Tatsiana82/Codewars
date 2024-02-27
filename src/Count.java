84677b6a4739ca4a752da9551d3ba7f899d17da2
Tatsiana82 <70721957+Tatsiana82@users.noreply.github.com>
2024-02-27 22:37:46 +0300

    Add

---
+++ src/Count.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/59342039eb450e39970000a6/train/java
+public class Count {
+    public static void main(String[] args) {
+        System.out.println(Kata.oddCount(5));
+    }
+    public class Kata {
+
+        public static int oddCount(int n){
+
+            return n / 2;
+        }
+    }
+}
