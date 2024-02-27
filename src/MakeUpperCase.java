f5d3a2ee303946b08f73d27c499d659af3c14a7a
Tatsiana82 <70721957+Tatsiana82@users.noreply.github.com>
2024-02-27 20:15:10 +0300

    Add

---
+++ src/MakeUpperCase.java
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+
+public class MakeUpperCase {
+    public static void main(String[] args) {
+        System.out.println(Upper.MakeUpperCase("Hello"));
+    }
+        class Upper{
+            public static String MakeUpperCase(String str){
+                return (str.toUpperCase());
+            }
+        }
+
+    }
+
+
